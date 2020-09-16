093f1fc09b18f70af1fc43b9d8f66ea132f562cd
Anthony Townsend <anthony@starcitygorup.us>
2020-09-16 15:20:47 -0400

    ready to start work on MonitoredVehicle parser

--- Database.py
+++ Database.py
@@ -3,7 +3,76 @@ import json
 import datetime
 
 
-def dump_api_response(data,route):
+'''
+from sqlalchemy import *
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+# todo how to init each database once and only once?
+# create tables
+Base.metadata.create_all(engine)
+
+class BusObservation(Base):
+    """"""
+    __tablename__ = "buses"
+
+    #todo from http://bustime.mta.info/wiki/Developers/SIRIMonitoredVehicleJourney
+    id = Column(Integer, primary_key=True)
+    vehicle_id = Column(String) # VehicleRef
+    route = Column(String,10)
+    lat=Column(Float) # VehicleLocation.Latitude
+    lon=Column(Float) # VehicleLocation.Longitude
+    bearing= Column(Float) # compass bearing, 0 = east, increments counterclockwise
+    routename_short=Column(String) # PublishedLineName
+    service_date=Column(Date) # DataFrameRef
+    agency = Column(String)# OperatorRef
+    destination = Column(String) # DestinationName
+
+    # todo store everything else i can
+    # todo optional to parse, add: ProgressRate, ProgressStatus, Occupancy,
+
+
+    #todo
+    def __init__(self, username, firstname, lastname, university):
+        """"""
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.university = university
+
+def get_session(route):
+
+    engine = create_engine('sqlite:///data/{}_buses.db'.format(route), echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+
+def dump_to_table(data, route):
+
+    session=get_session(route)
+
+    for b in data:
+
+        user = BusObservation("james", "James", "Boogie", "MIT") #todo
+        session.add(user)
+
+    # commit the session the database
+    session.commit()
+
+    return
+'''
+
+
+
+def dump_to_json(data, route):
 
     # You should change 'test' to your preferred folder.
     path = ("data/")
@@ -20,4 +89,10 @@ def dump_api_response(data,route):
     with open(dumpfile, 'w') as json_file:
         json.dump(data, json_file)
 
+    return
+
+
+def dump_to_screen(data,route):
+
+    print(json.dumps(data, indent=2))
     return
--- grabber.py
+++ grabber.py
@@ -1,9 +1,10 @@
-import os, sys
+import os
 import json
 from urllib.request import urlopen
 import argparse
 
-from Database import dump_api_response
+import Database as db
+import Parser as ps
 
 # load API KEY from .env (dont commit this file to the repo)
 from dotenv import load_dotenv
@@ -14,19 +15,24 @@ def get_feed(route):
     # fetch and prep feed
     # API reference http://bustime.mta.info/wiki/Developers/SIRIVehicleMonitoring
 
-    if args.route == "ALL":
+    if route == "ALL":
         url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + os.getenv(
             "API_KEY") + "&VehicleMonitoringDetailLevel=calls"
-        response = urlopen(url)
+        response = urlopen(url) # bug increase response timeout when route=ALL?
         data = response.read().decode("utf-8")
         data = json.loads(data)
+
     else:
-        url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + os.getenv(
-            "API_KEY") + "&VehicleMonitoringDetailLevel=calls&LineRef=" + args.route
+        url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + os.getenv("API_KEY") + "&VehicleMonitoringDetailLevel=calls&LineRef=" + route
         response = urlopen(url)
         data = response.read().decode("utf-8")
         data = json.loads(data)
-    dump_api_response(data,route)
+
+    if args.debug == True:
+        db.dump_to_screen(data,route)
+    db.dump_to_json(data, route)
+    # db.dump_to_table(data,route) #todo call ps.parse_buses
+
     return data
 
 
@@ -35,35 +41,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='NYCbuswatcher grabber, fetches and stores current position for buses')
     parser.add_argument('-r', action="store", dest="route") # a single route M1 or ALL
+    parser.add_argument('-d', action="store_true", dest="debug")
     args = parser.parse_args()
 
     data = get_feed(args.route)
     num_buses = len(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
     print ('grabber found {} on route(s) {}.'.format(num_buses,args.route))
 
-    for n in (range(0, num_buses)):
+    ps.parse_buses(data)
 
-        bus_lat = \
-            data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][n][
-                'MonitoredVehicleJourney'][
-                'VehicleLocation']['Latitude']
-        bus_lon = \
-            data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][n][
-                'MonitoredVehicleJourney'][
-                'VehicleLocation']['Longitude']
 
-        try:
-            bus_stop = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][n][
-                'MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
-        except LookupError:
-            bus_stop = "N/A"
-
-        try:
-            bus_status = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][n][
-                'MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances'][
-                'PresentableDistance']
-        except LookupError:
-            bus_status = "N/A"
-
-        result = ("%s\t%s\t%s %s" % (bus_lat, bus_lon, bus_status, bus_stop, ))
-        print(result)
