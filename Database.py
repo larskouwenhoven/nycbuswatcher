2ae0154b96e3e8dd70986b6bc7acbbe9d6421e7d
Anthony Townsend <anthony@starcitygorup.us>
2020-09-21 22:40:08 -0400

    ready to finish debugging database for async

--- Database.py
+++ Database.py
@@ -1,3 +1,6 @@
+import sys
+from datetime import datetime
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,6 +14,54 @@ def get_session(dbparams):
     Session = sessionmaker(bind=engine)
     session = Session()
     return session,db_url
+
+def parse_buses(route,data,db_url):
+    lookup = {'route_long':['LineRef'],
+              'direction':['DirectionRef'],
+              'service_date': ['FramedVehicleJourneyRef', 'DataFrameRef'],
+              'trip_id': ['FramedVehicleJourneyRef', 'DatedVehicleJourneyRef'],
+              'gtfs_shape_id': ['JourneyPatternRef'],
+              'route_short': ['PublishedLineName'],
+              'agency': ['OperatorRef'],
+              'origin_id':['OriginRef'],
+              'destination_id':['DestinationRef'],
+              'destination_name':['DestinationName'],
+              # 'scheduled_origin':['OriginAimedDepartureTime'], # appears to be omitted from feed
+              'alert': ['SituationRef', 'SituationSimpleRef'],
+              'lat':['VehicleLocation','Latitude'],
+              'lon':['VehicleLocation','Longitude'],
+              'bearing': ['Bearing'],
+              'progress_rate': ['ProgressRate'],
+              'progress_status': ['ProgressStatus'],
+              'occupancy': ['Occupancy'],
+              'vehicle_id':['VehicleRef'],
+              'gtfs_block_id':['BlockRef']
+              }
+    buses = []
+    try:
+        for b in data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']:
+            bus = BusObservation(route,db_url,datetime.now())
+            for k,v in lookup.items():
+                try:
+                    if len(v) > 1:
+                        val = b['MonitoredVehicleJourney'][v[0]][v[1]]
+                        setattr(bus, k, val)
+                    else:
+                        val = b['MonitoredVehicleJourney'][v[0]]
+                        setattr(bus, k, val)
+                except LookupError:
+                    pass
+                except Exception as e:
+                    print (e)
+                    pass
+            sys.stdout.write('.')
+            buses.append(bus)
+    except KeyError: #no VehicleActivity?
+        pass
+    return buses # bug nothing going to the database?
+
+
+
 
 Base = declarative_base()
 
--- grabber_async.py
+++ grabber_async.py
@@ -4,6 +4,13 @@
 # Using the homogeneous-session.
 import requests
 import os
+from datetime import datetime
+import json
+import sys
+import time
+import gzip
+
+import Database as db
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,28 +27,92 @@ def get_path_list():
         'Found {} routes. Fetching current positions with ASYNCHRONOUS requests...'.format(len(routes['data']['list'])))
     for route in routes['data']['list']:
         path_list.append(
-            "/api/where/routes-for-agency/MTA%20NYCT.json?key={}&VehicleMonitoringDetailLevel=calls&LineRef={}".format(
-                os.getenv("API_KEY"), route['id']))
-    return path_list
+            {route['id']:"/api/where/routes-for-agency/MTA%20NYCT.json?key={}&VehicleMonitoringDetailLevel=calls&LineRef={}".format(os.getenv("API_KEY"), route['id'])}
+        )
+    return path_list #bug host not found error bustime.mta.info, catch wait 3-5 sec and try again
 
 
-path_list = get_path_list()
+def dump_to_screen(feeds):
+    for route_bundle in feeds:
+        for route_id, data in route_bundle.items():
+            print (data.json())
+    return
 
-results = []
+def dump_to_file(feeds):
+    path = ("data/")
+    check = os.path.isdir(path)
+    if not check:
+        os.makedirs(path)
+        print("created folder : ", path)
+    else:
+        pass
+    date = datetime.now().strftime("%Y-%m-%dT_%H:%M:%S.%f")
+    for route_bundle in feeds:
+        for route_id,route_report in route_bundle.items():
 
-async def grabber(s,a_path):
-    r = await s.get(path=a_path)
-    results.append(r)
+            # uncompressed version
+            dumpfile=(path + route_id.split()[1] + '_' + date +'.json')
+            with open(dumpfile, 'w') as json_file:
+               json.dump(route_report.json(), json_file, indent=4)
 
-async def main(path_list):
-    from asks.sessions import Session
-    s = Session('http://bustime.mta.info', connections=20)
-    async with trio.open_nursery() as n:
-        for path in path_list:
-            n.start_soon(grabber, s, path)
+            # todo add compression
+            # https://medium.com/@busybus/zipjson-3ed15f8ea85d
+
+    return
+
+def dump_to_db(dbparams,feeds): # todo debug database
+
+    session,db_url = db.get_session(dbparams)
+    print('Dumping to {}'.format(db_url))
+    for route_bundle in feeds:
+        # sys.stdout.write(' <{}> '.format(str(next(iter(route_bundle)))))
+        for route_id,route_report in route_bundle.items():
+            for bus in db.parse_buses(route_id, route_report.json(), db_url):
+                session.add(bus)
+        session.commit()
+    return
 
 
-trio.run(main, path_list)
 
-print(results)
+
+if __name__ == "__main__":
+    start = time.time()
+
+    # todo add production switch + chane dbhost to 'mysql_docker'
+    # todo add scheduler
+
+    dbparams = {
+        'dbname': 'buses',
+        'dbuser': 'nycbuswatcher',
+        'dbpassword': 'bustime',
+        'dbhost':'localhost'
+    }
+
+    path_list = get_path_list()
+
+    feeds = []
+
+    async def grabber(s,a_path,route_id):
+        r = await s.get(path=a_path)
+        feeds.append({route_id:r})
+
+    async def main(path_list):
+        from asks.sessions import Session
+        s = Session('http://bustime.mta.info', connections=20)
+        async with trio.open_nursery() as n:
+            for path_bundle in path_list:
+                for route_id,path in path_bundle.items():
+                    n.start_soon(grabber, s, path, route_id )
+
+
+    trio.run(main, path_list) # bug ValueError: 'bustime.mta.info' does not appear to be an IPv4 or IPv6 address
+
+    # dump_to_screen(feeds)
+    dump_to_file(feeds)
+    dump_to_db(dbparams, feeds)
+
+    end = time.time()
+    print('\nFetched {} routes in {:2f} seconds to uncompressed archive and database.'.format(len(feeds),(end - start)))
+
+
 
